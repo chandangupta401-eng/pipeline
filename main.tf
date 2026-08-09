47f7c193a75b09236988a29a686e6428bef012b9
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-09 22:25:43 +0530

    removerg1

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 
-
-resource "azurerm_resource_group" "FRI1" {
-  name     = "day5"
-  location = "Central India"
+provider "azurerm" {
+  features {
+    #depends_on = [azurerm_resource_group.RG1]
+  }
 }
+
+
 
 
 
