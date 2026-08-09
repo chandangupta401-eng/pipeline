2e87a8462aaa667d9128ea4cae583554d4b9f500
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-09 22:18:56 +0530

    storblob1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "day5"
     storage_account_name = "iinspirabb8787jj"
-    container_name       = "tfstate"
+    container_name       = "ds256"
     key                  = "terraform.tfstate"
   }
 }
