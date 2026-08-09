f4b2d4cf8d34ac39a00c859ec9855a18e58523f9
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-09 13:42:07 +0530

    Format Terraform files1

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "RG1" {
   name     = "RGTest"
   location = "CentralIndia"
 }
+
+provider "azurerm" {
+  features {
+    #depends_on = [azurerm_resource_group.RG1]
+  }
+}
