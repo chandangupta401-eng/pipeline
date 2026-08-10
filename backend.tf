860fdfae8c30f89fdfe339d47840ab955fd4e9aa
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-10 13:03:20 +0530

    feature/rg-vnet

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "day5"
-    storage_account_name = "iinspirabb8787jj"
-    container_name       = "ds256"
-    key                  = "terraform.tfstate"
-  }
-}
--- main.tf
+++ main.tf
@@ -6,22 +6,14 @@ provider "azurerm" {
 }
 
 
-
-
-
-resource "azurerm_storage_account" "FRI2" {
-  depends_on               = [azurerm_resource_group.FRI1]
-  name                     = "iinspirabb8787jj"
-  resource_group_name      = "day5"
-  location                 = "Central India"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_resource_group" "RGPipe" {
+  name     = "RG1"
+  location = "CentralIndia"
 }
 
-resource "azurerm_storage_container" "con1" {
-  depends_on = [azurerm_storage_account.FRI2]
-  name       = "ds256"
-
-  storage_account_id    = azurerm_storage_account.FRI2.id
-  container_access_type = "blob"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "virtualnetwork"
+  resource_group_name = azurerm_resource_group.RGPipe.name
+  location            = azurerm_resource_group.RGPipe.location
+  address_space       = ["10.0.0.0/16", "192.168.1.0/24"]
 }
