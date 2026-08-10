3841b646d046b979be374ad02d286c14f050620e
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-10 15:15:19 +0530

    subne2

--- main.tf
+++ main.tf
@@ -22,5 +22,5 @@ resource "azurerm_subnet" "sub" {
   name                 = "sub"
   resource_group_name  = azurerm_resource_group.RGPipe.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.10.0/24"]
+  address_prefixes     = ["10.0.10.0/24"]
 }
