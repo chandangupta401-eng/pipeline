48548d009c20a1d7be091ebb3df620b98f97fcf6
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-10 15:10:36 +0530

    udated

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "RG"
     storage_account_name = "storagetest10aug26"
     container_name       = "blob"
-    key                  = "terraform.tfstate"
+    key                  = "terraform1.tfstate"
   }
 }
--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 
 
 resource "azurerm_resource_group" "RGPipe" {
-  name     = "RG1"
+  name     = "RG2"
   location = "CentralIndia"
 }
 
@@ -18,9 +18,3 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16", "192.168.1.0/24"]
 }
 
-resource "azurerm_subnet" "sub" {
-  name                 = "sub"
-  resource_group_name  = azurerm_resource_group.RGPipe.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.10.0.0/24"]
-}
