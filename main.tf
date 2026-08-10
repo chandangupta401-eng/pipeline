b352f694e9d1bd0e4c6278abd936a36f82d4de4d
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-10 15:18:36 +0530

    romoverg

--- main.tf
+++ main.tf
@@ -6,10 +6,7 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "RGPipe" {
-  name     = "RG2"
-  location = "CentralIndia"
-}
+
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "virtualnetwork"
