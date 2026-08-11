b55e7cef62960e358c2894e99751229b46bb5bbb
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-11 12:27:56 +0530

    destroy

--- main.tf
+++ main.tf
@@ -6,21 +6,21 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "RGPipe" {
-  name     = "RG2"
-  location = "CentralIndia"
-}
+# resource "azurerm_resource_group" "RGPipe" {
+#   name     = "RG2"
+#   location = "CentralIndia"
+# }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "virtualnetwork"
-  resource_group_name = azurerm_resource_group.RGPipe.name
-  location            = azurerm_resource_group.RGPipe.location
-  address_space       = ["10.0.0.0/16", "192.168.1.0/24"]
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = "virtualnetwork"
+#   resource_group_name = azurerm_resource_group.RGPipe.name
+#   location            = azurerm_resource_group.RGPipe.location
+#   address_space       = ["10.0.0.0/16", "192.168.1.0/24"]
+# }
 
-resource "azurerm_subnet" "sub" {
-  name                 = "sub"
-  resource_group_name  = azurerm_resource_group.RGPipe.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.10.0/24"]
-}
+# resource "azurerm_subnet" "sub" {
+#   name                 = "sub"
+#   resource_group_name  = azurerm_resource_group.RGPipe.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = ["10.0.10.0/24"]
+# }
