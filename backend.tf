efa92aeb66771aae71de56548e85899b2a8a20fd
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-11 12:15:59 +0530

    brnach1

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "RG"
-    storage_account_name = "storagetest10aug26"
-    container_name       = "blob"
-    key                  = "terraform1.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "RG"
+#     storage_account_name = "storagetest10aug26"
+#     container_name       = "blob"
+#     key                  = "terraform1.tfstate"
+#   }
+# }
