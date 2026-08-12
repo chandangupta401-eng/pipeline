61d5b5029c4e5d047d79ae790bf952677adf66a7
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-12 12:56:24 +0530

    qwerty

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 variable "rg" {}
 variable "storage" {}
+variable "vnet" {}
 module "RG" {
   source = "./moduel/pre/RG"
   rg     = var.rg
 }
-module "storage" {
-  source = "./moduel/pre/Storage"
-  storage = {
-    name                     = var.storage.name
-    resource_group_name      = module.RG.name
-    location                 = module.RG.location
-    account_tier             = var.storage.account_tier
-    account_replication_type = var.storage.account_replication_type
+
+module "network" {
+  source = "./moduel/pre/vnet"
+  vnet = {
+    name                = var.vnet.name
+    resource_group_name = module.RG.name
+    location            = module.RG.location
   }
 }
--- moduel/pre/Storage/main.tf
+++ moduel/pre/Storage/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage" {
-  name = var.storage.name
+  name                     = var.storage.name
   resource_group_name      = var.storage.resource_group_name
   location                 = var.storage.location
   account_tier             = var.storage.account_tier
