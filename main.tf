734ffc32636cc650caa6347e99ec1a94b76ef3f2
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-14 13:18:34 +0530

    Test11

--- main.tf
+++ main.tf
@@ -26,3 +26,15 @@ module "subnet" {
 
   }
 }
+
+module "storage" {
+  source ="./moduel/pre/Storage"
+  storage = {
+    name = var.storage.name
+    resource_group_name = module.RG.name
+    location = module.RG.location
+    account_tier             = var.storage.account_tier
+    account_replication_type = var.storage.account_replication_type
+    
+  }
+}
