0ab7afc88614d417917af9d1329bd17278fcf73f
chandangupta401-eng <chandan.gupta401@gmail.com>
2026-08-21 17:11:01 +0530

    upodateyml

--- prv.tf
+++ prv.tf
@@ -1,12 +1,14 @@
 terraform {
+  required_version = "~> 1.15.7"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      
+      version = "= 4.79.0"
     }
   }
 }
+
 provider "azurerm" {
-  # Configuration options
   features {}
 }
